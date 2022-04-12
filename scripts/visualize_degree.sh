a3c821890d39c2a4dd000598fa824d696c363ac4
York1998 <50006186+York1998@users.noreply.github.com>
2022-04-12 17:30:56 +0800

    Initial commit

---
+++ scripts/visualize_degree.sh
@@ -0,0 +1,4 @@
+python -m utils.visualize_anchor_degree --source_dataset dataspace/douban/online/graphsage --target_dataset dataspace/douban/offline/graphsage --groundtruth dataspace/douban/dictionaries/groundtruth
+
+
+python -m utils.visualize_anchor_degree --source_dataset dataspace/ppi/graphsage --target_dataset dataspace/ppi/REGAL-d05-seed1/graphsage --groundtruth dataspace/ppi/REGAL-d05-seed1/dictionaries/groundtruth
